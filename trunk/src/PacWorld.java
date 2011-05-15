f00317d123df37859ebb3666fa5d124575e0b6e4
mpolevy@gmail.com <mpolevy@gmail.com@bcbeb2da-f6ca-07f5-7c0f-616aa8cb8f86>
2011-05-15 01:49:27 +0000

    Added the GridWorld framework so that it can be accessed from anywhere.

---
+++ trunk/src/PacWorld.java
@@ -0,0 +1,20 @@
+import info.gridworld.world.World;
+
+public class PacWorld extends World{
+	
+	/**
+	 * @author Max
+	 * 
+	 * The PacWorld contains the grid and the actors. It's a "game" object. Controls
+	 * the game environment by taking inputs and modifying the actors based on those
+	 * inputs.
+	 */
+	
+	//constructor doesn't need anything
+	public PacWorld(){
+		
+	}
+	
+	
+
+}
