67575258d21339aa3a2475deabb8389f86d0b204
mooney.jake@gmail.com <mooney.jake@gmail.com@bcbeb2da-f6ca-07f5-7c0f-616aa8cb8f86>
2011-05-18 13:15:29 +0000

    1. Pacman works all the time to best of my knowledge except when it encounters a boundary of the grid. ghosts havent been tested.

--- framework/info/gridworld/actor/Ghost.java
+++ framework/info/gridworld/actor/Ghost.java
@@ -1,11 +1,68 @@
 package info.gridworld.actor;
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.*;
+
 import java.awt.Color;
-
-public class Ghost extends Actor{
-	
-	public Ghost(Color c){
-		setColor(c);
+import java.util.*;
+public class Ghost extends Critter{
+	private static boolean vulnerable;
+	private Actor covered;
+	public Ghost(){
+		vulnerable = true;
 	}
-
+	public Ghost(Color c){
+		this.setColor(c);
+	}
+	public static boolean isvulnerable(){
+		return vulnerable;
+	}
+	public static void setVulnerable(boolean a){
+		vulnerable = a;
+	}
+	public void act(){
+		Location target = FindPacMan();
+		if (target == null)
+			return;
+		else{
+			Location loc = getMoveLocation(target);
+			
+			moveTo(getMoveLocation(loc));	
+		}
+	}
 	
+	public Location FindPacMan(){
+		ArrayList<Location> locs  = getGrid().getOccupiedLocations();
+		for (Location l: locs){
+			if (getGrid().get(l) instanceof PacMan)
+				return l;
+		}
+		return null;
+	}
+	public Location getMoveLocation(Location target){ 
+		
+		//currently can only move to empty spots, must be able to move over pellets
+		
+		
+		int dir = this.getLocation().getDirectionToward(target);
+		if (vulnerable)
+			dir += 180;
+		Location loc = getLocation().getAdjacentLocation(dir);
+		if (getGrid().get(loc).equals(null))
+			return loc;
+		return selectMoveLocation(getMoveLocations());  
+	}
+	public void move(Location loc){
+		if (getGrid().get(loc).equals(null)){
+			Location loc2 = this.getLocation();
+			moveTo(loc);
+		//	if (!covered.equals(null))
+				//covered.putSelfInGrid(loc2);
+			
+		}
+		else{
+			covered = getGrid().get(loc);
+		}
+	}
 }
+
