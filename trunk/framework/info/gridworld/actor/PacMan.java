2ce5a2d8e07c49d66549b1779291395636c9d98a
mooney.jake@gmail.com <mooney.jake@gmail.com@bcbeb2da-f6ca-07f5-7c0f-616aa8cb8f86>
2011-05-18 13:15:29 +0000

    1. Pacman works all the time to best of my knowledge except when it encounters a boundary of the grid. ghosts havent been tested.

--- trunk/framework/info/gridworld/actor/PacMan.java
+++ trunk/framework/info/gridworld/actor/PacMan.java
@@ -1,34 +1,69 @@
 package info.gridworld.actor;
 
+import info.gridworld.actor.Actor;
 import info.gridworld.grid.Grid;
 import info.gridworld.grid.Location;
 
 import java.awt.Color;
-
-public class PacMan extends Bug{ //?????? OK, whatever - Vivek
-	
+import java.util.ArrayList;
+public class PacMan extends Actor{
+	private int points, count;
+	private static int lives;
+	private boolean vulnerable, dead;
 	public PacMan(){
-		setColor(Color.yellow);
+		lives = 3;
+		points = 0;
+		count = 21;
+		vulnerable = false;
+		dead = false;
+		setColor(Color.YELLOW);
 	}
 	
-	/*public void act(){
-		if (canMove())
-			move();
-		else
-			removeSelfFromGrid();
-	}*/
+	public void act(){
+		if (count == 20)
+			Ghost.setVulnerable(false);
+		count++;
+		int dir = getDirection();
+		Location loc = getLocation().getAdjacentLocation(dir);
+		Actor a = getGrid().get(loc);
+		if (a instanceof MazeWall )
+			return;
+		else if (a instanceof Pellet){
+			points += 10;
+			a.removeSelfFromGrid();
+			this.moveTo(loc);
+		}
+		else if (a instanceof Ghost && ((Ghost)a).isvulnerable()){
+			points += 200;
+			a.removeSelfFromGrid();
+			this.moveTo(loc);
+		}
+		else if (a instanceof Ghost){
+			dead = true;
+			lives--;
+			this.removeSelfFromGrid();
+		}
+		else if (a instanceof PowerPellet1 || a instanceof PowerPellet2){
+			points += 50;
+			a.removeSelfFromGrid();
+			this.moveTo(loc);
+			count = 0;
+			Ghost.setVulnerable(true);
+		}
+		//else if (loc.isValid())
+		else //if (a.equals(null))
+			moveTo(loc);
+	}
+	public boolean isDead(){
+		return dead;
+	}
+	public static void kill(){
+		lives--;
+	}
 	
-	/*just testing code, replace with your own class please jake
-	public void move(){
-		 Grid<Actor> gr = getGrid();
-	        if (gr == null)
-	            return;
-	        Location loc = getLocation();
-	        Location next = loc.getAdjacentLocation(getDirection());
-	        if (gr.isValid(next))
-	            moveTo(next);
-	        else
-	            removeSelfFromGrid();
-	}*/
+	
+	
+	
 
 }
+
