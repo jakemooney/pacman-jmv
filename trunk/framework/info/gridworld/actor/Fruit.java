304bbbee1d0e0b763ea612cf8ef2941aec48384b
xadxad@gmail.com <xadxad@gmail.com@bcbeb2da-f6ca-07f5-7c0f-616aa8cb8f86>
2011-05-21 19:46:51 +0000

    Am going to finish fruit later. Testing if this works from my mac.

---
+++ trunk/framework/info/gridworld/actor/Fruit.java
@@ -0,0 +1,13 @@
+package info.gridworld.actor;
+
+//Vivek G - 5/21/11 - My attempt at making fruit
+
+/*
+ * Notes:
+ * -Fruit only appears under the cages of the ghosts
+ * -this only 
+ */
+
+public class Fruit {
+
+}
