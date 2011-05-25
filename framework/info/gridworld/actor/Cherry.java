b31c7c3da9b3ce3edfa7e79a022e6c3e61391821
xadxad@gmail.com <xadxad@gmail.com@bcbeb2da-f6ca-07f5-7c0f-616aa8cb8f86>
2011-05-25 17:38:10 +0000

    Commit?

---
+++ framework/info/gridworld/actor/Cherry.java
@@ -0,0 +1,12 @@
+package info.gridworld.actor;
+
+import java.awt.Color;
+
+public class Cherry extends Fruit{
+
+	public Cherry(){
+		super(100);
+		//setColor(Color.TRANSLUCENT);					???????
+	}
+	
+}
